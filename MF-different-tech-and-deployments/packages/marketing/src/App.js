a77cab5fe587f993db90ad1d75fe00a3764b3eb3
mansi-4124 <shintrems4@gmail.com>
2026-04-24 13:48:18 +0530

    randomize production classnames for css scoping

--- MF-different-tech-and-deployments/packages/marketing/src/App.js
+++ MF-different-tech-and-deployments/packages/marketing/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Switch, Router, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 
+const generateClassName = createGenerateClassName({ productionPrefix: "ma" });
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
